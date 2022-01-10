66865c181e1e99ad77fd24f4ce81f73da5d31c08
mnangwe <pupuma.azabenathi@gmail.com>
2022-01-10 13:43:55 +0200

    Second part

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 let carList = document.getElementById('cars');
-let cars = ['Abarth', 'Acura', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Buick', 'Chevrolet', 'Ferrari', 'Ford', 'GMC', 'Jaguar', 'Mercedes-Benz', 'Toyota'];
-  
+//let cars = ['Abarth', 'Acura', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Buick', 'Chevrolet', 'Ferrari', 'Ford', 'GMC', 'Jaguar', 'Mercedes-Benz', 'Toyota'];
+
+let cars = [
+    {
+    name: 'Abarth',
+    founder: 'Carlo Abarth',
+    year: 1949 
+    },
+    {
+        name: 'Acura',
+        founder: 'Soichiro Honda',
+        year: 1986  
+    },
+    {
+        name: 'Alfa Romeo',
+        founder: 'Nicola Romeo',
+        year: 1910  
+    },
+    {
+        name: 'Aston Martin',
+        founder: 'Lionel Martin',
+        year: 1913  
+    },
+    {
+        name: 'Audi',
+        founder: 'August Horch',
+        year: 1909 
+    },
+    {
+        name: 'BMW',
+        founder: 'Gustav Otto',
+        year: 1916 
+    },
+    {
+        name: 'Jaguar',
+        founder: 'William Lyons',
+        year: 1922 
+    },
+    {
+        name: 'Mercedes-Benz',
+        founder: 'Karl Benz',
+        year: 1926 
+    },
+    {
+        name: 'Toyota',
+        founder: 'Kiichiro Toyoda',
+        year: 1937 
+    }
+]
+
 countCars = data => {
   let count = document.getElementById('counter');
 
@@ -12,7 +60,7 @@ countCars = data => {
   }
 };
 
-countCars(cars.length);
+
 
 //Display the List of Cars
   getCars = () => {
@@ -20,45 +68,81 @@ countCars(cars.length);
     if (cars.length > 0) {
       for (i = 0; i < cars.length; i++) {
         data += `<tr>
-        <td> ${cars[i]} </td>\
-         <td><button onclick="editCar( ${i} )">Edit</button></td>\
-         <td><button onclick="deleteCar( ${i} )">Delete</button></td>\
+        <td> ${cars[i].name} </td>
+        <td> ${cars[i].founder} </td>
+        <td> ${cars[i].year} </td>
+         <td><button onclick="editCar( ${i} )">Edit</button></td>
+         <td><button onclick="deleteCar( ${i} )">Delete</button></td>
          </tr>`;
       }
     }
-  
+    countCars(cars.length);
   return carList.innerHTML = data;
 };
 // Create: POST
 addCar = () => {
-  let carAdded = document.getElementById('add-name');
-  // Get the value
-  let car = carAdded.value;
+  let nameAdd = document.getElementById('add-name');
+  let founderAdd = document.getElementById('add-founder');
+  let yearAdd = document.getElementById('add-year');
 
-  if (car) {
+    // Object
+    let car = {
+        name: nameAdd.value.trim(),
+        founder:founderAdd.value.trim(),
+        year: yearAdd.value.trim()
+      }
+
+  // Get the value
+  let name = nameAdd.value;
+  let founder = founderAdd.value;
+  let year = yearAdd.value;
+
+  if (name && founder && year) {
     // addCar the new value
-    cars.push(car.trim());
+    cars.push(car);
+    console.log(cars)
     // Reset input value
-    carAdded.value = '';
+    nameAdd.value = '';
+    founderAdd.value = '';
+    yearAdd.value = '';
     // Dislay the new list
     getCars();
   }
 };
+
+
 // Update: PUT
 editCar = item => {
-  let editCar = document.getElementById('edit-name');
+  let editName = document.getElementById('edit-name');
+  let editFounder = document.getElementById('edit-founder');
+  let editYear = document.getElementById('edit-year');
   // Display value in the field
-  editCar.value = cars[item];
+
+  editName.value = cars[item].name;
+  editFounder.value = cars[item].founder;
+  editYear.value = cars[item].year;
+
   // Display fields
   document.getElementById('editForm').style.display = 'block';
 
   document.getElementById('saveEdit').onsubmit = () => {
     // Get value
-    let car = editCar.value;
-    if (car) {
+    let name = editName.value;
+    let founder = editFounder.value;
+    let year = editYear.value;
+
+    //Object
+    let car = {
+        name:name, 
+        founder:founder, 
+        year:year
+    }
+
+    if (name && founder && year) {
       // editCar value
-      cars.splice(item, 1, car.trim());
+      cars.splice(item, 1, car);
       // Display the new list
+      console.log(cars)
       getCars();
       // Hide fields
       closeInput();
@@ -70,6 +154,7 @@ deleteCar = item => {
   // deleteCar the current row
   cars.splice(item, 1);
   // Display the new list
+  console.log(cars)
   getCars();
 };
 
@@ -78,4 +163,6 @@ getCars();
 closeInput = () => {
   document.getElementById('editForm').style.display = 'none';
 }
+
+
 
